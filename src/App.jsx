a197e09c7f9d8896d8cd1a336d5a4f8d1d031074
Timur <m2205151@edu.misis.ru>
2025-03-12 17:15:38 +0300

    sales & orders pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { Route, Routes } from "react-router-dom"
 import OverviewPage from "./pages/OverviewPage"
 import ProductsPage from "./pages/ProductsPage"
 
-import Sidebar from "./components/Sidebar"
+import Sidebar from "./components/common/Sidebar"
 import UsersPage from "./pages/UsersPage"
+import SalesPage from "./pages/SalesPage"
+import OrdersPage from "./pages/OrdersPage"
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
         <Route path='/' element={<OverviewPage />} />
         <Route path='/products' element={<ProductsPage />} />
         <Route path='/users' element={<UsersPage />} />
+        <Route path='/sales' element={<SalesPage />} />
+        <Route path='/orders' element={<OrdersPage />} />
       </Routes>
     </div>
   )
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -21,7 +21,6 @@ const UsersPage = () => {
 			<Header title='Users' />
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
-				{/* STATS */}
 				<motion.div
 					className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
 					initial={{ opacity: 0, y: 20 }}
@@ -46,12 +45,11 @@ const UsersPage = () => {
 
 				<UsersTable />
 
-				<UserActivityHeatmap />
-				<UserDemographicsChart />
-				<UserGrowthChart />
-
-				{/* USER CHARTS */}
 				<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8'>    
+					<UserGrowthChart />
+					<UserActivityHeatmap />
+					<UserDemographicsChart />
+					
 				</div>
 			</main>
 		</div>
